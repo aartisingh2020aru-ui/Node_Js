20a0e6566ee850b438ff319435c7ff211439ea6b
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-05-13 12:03:22 +0530

    math/text/document/subtract/add

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ setTimeout(()=>{
 
 console.log("End");
 
+
+
+const math = require('./math');
+console.log(math.add(4,6));
+console.log(math.sub(4,6));
+
+
+
+const fs = require('fs');
+fs.readFile('data.txt', 'utf-8', (err, data)=>{
+    console.log(data);
+});
