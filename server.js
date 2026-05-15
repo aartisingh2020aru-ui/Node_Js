f132edbace032db6ec8dd5bb8adb797f7ec10aae
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-05-15 12:17:14 +0530

    server making

---
+++ server.js
@@ -0,0 +1,13 @@
+
+
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    res.write("Bhag Yaha Se!");
+    res.end();
+});
+
+server.listen(8000, ()=>{
+    console.log("Server is running on port 8000");
+});
+
