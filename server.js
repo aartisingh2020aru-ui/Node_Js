96b3ad88af25d31b6299a0ce40f7401f923ecc0b
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-05-16 11:56:07 +0530

    router

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 
 
+// const http = require('http');
+
+// const server = http.createServer((req, res)=>{
+//     res.write("Bhag Yaha Se!");  
+//     res.end();
+// });
+
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    res.write("Bhag Yaha Se!");
-    res.end();
+    if(req.url === '/'){
+        res.end("HOME PAGE");
+
+    }else if(req.url === '/about'){
+        res.end("ABOUT PAGE");
+
+    }else{
+        res.end("404 page not found!!");
+    }
 });
 
 server.listen(8000, ()=>{
